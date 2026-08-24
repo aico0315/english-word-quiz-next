07ccbcaa8ea3599bf7aab98cbd4637bf042eb800
aico0315 <aiko.phoenix12@gmail.com>
2026-08-24 09:11:00 +0900

    feat: add CategorySelect and its dependencies
    add Button.tsx and CategorySelect.tsx to src/components
    add getSortedCategories.ts to src/utils
    install motion package

---
+++ src/components/Button.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import "/src/components/Button.css";
+
+interface ButtonProps {
+  label: string | React.ReactElement;
+  className: string;
+  variant?: string;
+  onUpdate?: (label: string | React.ReactElement)=> void;
+  onPhaseChange?: ()=> void;
+}
+
+export default function Button({label, className, variant, onUpdate, onPhaseChange }: ButtonProps){
+  return(
+      <button
+        className={`${className} ${variant}`}
+        onClick={
+          onUpdate && onPhaseChange ? () => { onUpdate(label); onPhaseChange(); }
+          : onUpdate ? ()=> onUpdate(label)
+          : onPhaseChange }>
+          {label}
+      </button>
+  )
+}
